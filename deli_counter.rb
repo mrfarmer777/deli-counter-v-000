d10362d81850c4074e68e13b769c02f432181e40
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 05:28:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,25 +3,25 @@ katz_deli=[]
 
 def line(array)
   if array.length==0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     result = "The line is currently: "
     array.each_with_index do |name,ind|
       result<<"#{ind+1}. #{name}"
     end
-    return result
+    puts result
   end
 end
 
 def take_a_number(array,name)
   array.push(name)
-  return "Welcome #{name}. You are number #{array.index(name)+1} in line."
+  puts "Welcome #{name}. You are number #{array.index(name)+1} in line."
 end
 
 def now_serving(array)
   if array.size>0
-    return "Currently serving #{array.unshift}."
+    puts "Currently serving #{array.unshift}."
   else
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
 end
