a4eee7c6e575c7057f19558531aa93d580fbcd14
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 05:17:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli=[]
+def line(array)
+  length=array.size
+  case length
+  when 0
+    return "The line is currently empty."
+  else
+    result = "The line is currently: "
+    array.each_with_index do |name,ind|
+      result<<"#{ind+1}. #{name}"
+    return result
